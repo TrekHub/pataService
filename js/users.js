892094cb50421022fccd66662e2f2f7452b25080
mohraf <amosngok@gmail.com>
2021-12-01 15:28:50 +0300

    Add auth logic

--- js/users.js
+++ js/users.js
@@ -1,55 +1,16 @@
-function Customer (id, firstName, lastName, email, password, confirmPassword, phoneNumber) {
-  this.id = id;
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.email = email;
-  this.password = password;
-  this.confirmPassword = confirmPassword;
-  this.phoneNumber = phoneNumber;
-};
-
-let customers = [];
-
-$('#create').on('click', () => {
-  let firstName = $("#firstName").val();
-  let lastName = $("#lastName").val();
-  let id = firstName;
-  let email = $("#email").val();
-  let password = $("#password").val();
-  let confirmPassword = $("#confirmPassword").val();
-  let phoneNumber = $("#phoneNumber").val();
-  // console.log(firstName);
-
-  let myCustomer;
-
-  if (password === confirmPassword){
-    myCustomer = new Customer(id, firstName, lastName, email, password, confirmPassword, phoneNumber);
-    customers.push(myCustomer);
-    window.location.href = '../index.html';
-  } else {
-    alert("Your Passwords didn't match");
-  }
-
-  // console.log(myCustomer);
-  // console.log(customers);
-});
+const admin = {
+  userName: "admin",
+  email: "admin@mail.com",
+  password: "admin123" 
+}
 
 $('#login').on('click', () => {
   let loginEmail = $("#loginEmail").val();
   let loginPassword = $("#loginPassword").val();
 
-  console.log(loginEmail);
-
-  // let i = customers.length;
-  let loginUser = customers.find(customer => {
-    return customer.email === loginEmail;
-  });
-
-  console.log(loginUser);
-
-  if (loginPassword === loginUser.password){
+  if ((loginPassword === admin.password) && (loginEmail === loginEmail)){
     window.location.href = '../index.html';
   } else {
-    alert("Invalid Password");
+    alert("Invalid Email or Password");
   }
 });
